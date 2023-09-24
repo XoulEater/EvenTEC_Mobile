58ef0a2001c62f7302a79b0cb33a79459a9b89ec
David Fernandez <josfernandez@estudiantec.cr>
2023-09-23 18:17:56 -0600

    registroEstudiante

---
+++ app/src/main/java/com/example/eventec/RegistroE.java
@@ -0,0 +1,14 @@
+package com.example.eventec;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistroE extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_e);
+    }
+}
