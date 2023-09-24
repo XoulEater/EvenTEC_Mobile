e40f3abc6477abb5cc2213e6ca84ea34d4c8a9b5
whiit <ncqueescribir@gmail.com>
2023-09-24 11:52:36 -0600

    login/register update

---
+++ app/src/main/java/com/example/eventec/Registro.java
@@ -0,0 +1,25 @@
+package com.example.eventec;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registro extends AppCompatActivity {
+    private int currentUserType;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        currentUserType = getIntent().getIntExtra("userType", 0);
+
+        switch (currentUserType) {
+            case 1:
+                setContentView(R.layout.activity_registro_c);
+                break;
+            case 2:
+                setContentView(R.layout.activity_registro_a);
+                break;
+            default:
+                setContentView(R.layout.activity_registro_e);
+        }
+    }
+}
