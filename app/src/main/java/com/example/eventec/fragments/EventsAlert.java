80e53b716745a7710e1f58444ef12cb4161a5c57
whiit <ncqueescribir@gmail.com>
2023-09-27 16:36:24 -0600

    Event Creator

--- app/src/main/java/com/example/eventec/fragments/EventsAlert.java
+++ app/src/main/java/com/example/eventec/fragments/EventsAlert.java
@@ -18,6 +18,6 @@ public class EventsAlert extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_event_alerts, container, false);
+        return inflater.inflate(R.layout.fragment_events_alert, container, false);
     }
 }
--- app/src/main/java/com/example/eventec/fragments/EventsCreator.java
+++ app/src/main/java/com/example/eventec/fragments/EventsCreator.java
@@ -1,22 +1,58 @@
 package com.example.eventec.fragments;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.example.eventec.R;
 
 
 public class EventsCreator extends Fragment {
-
+    private final int GALLERY_REQ_CODE = 1000;
+    private Uri selectedImageUri;
+    private ImageView eventImage;
+    private ImageButton btnGallery;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_events_creator, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_events_creator, container, false);
+
+        eventImage = view.findViewById(R.id.eventImage);
+        btnGallery = view.findViewById(R.id.btnGallery);
+
+        btnGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent iGallery = new Intent(Intent.ACTION_PICK);
+                iGallery.setData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(iGallery, GALLERY_REQ_CODE);
+            }
+        });
+
+        return view;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GALLERY_REQ_CODE && resultCode == RESULT_OK){
+            selectedImageUri = data.getData();
+            eventImage.setImageURI(selectedImageUri);
+        }
     }
 }
